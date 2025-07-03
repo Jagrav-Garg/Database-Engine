04f0672be24401222609632904fd66bec25d3765
PrakharG8651 <prakhargupta1568@gmail.com>
2025-07-04 01:32:35 +0530

    Node structure completed

--- avl.h
+++ avl.h
@@ -6,7 +6,8 @@
 
 typedef struct{
     int roll;
-    
+    int value[2]; // value[0] is page number(page_index), value[1] is row number(slot_index)
+    Node *left,*right;// left and right pointers for AVL tree structure
 } Node;
 
 
