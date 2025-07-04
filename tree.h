f31bb97ce35fa9e7ce57c5f20376ed9f2d36824b
TimePass133 <dibyanshudas242.2010@gmail.com>
2025-07-04 18:35:26 +0530

    index_delete() function added

--- tree.h
+++ tree.h
@@ -17,6 +17,6 @@ typedef struct Node{
 
 void index_insert(Node** node,int key,int page,int slot);
 bool index_find(int key,int* page,int* slot);
-void index_delete(int key);
+void index_delete(Node** node, int key);
 // void print_backend_test(Node* root);
 #endif
